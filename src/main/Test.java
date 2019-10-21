2f4363b07afdbcaaaa5282ad62020b1f6284e2a7
Conor Shields <42182485+cshields236@users.noreply.github.com>
2019-10-21 01:21:58 +0100

    final commit
    Signed-off-by: Conor Shields <42182485+cshields236@users.noreply.github.com>

--- src/main/Test.java
+++ src/main/Test.java
@@ -14,120 +14,125 @@ public class Test {
 
 
     public Test() {
+
+        //Declarations
         mainDAO mainDAO = new mainDAO();
         List<Driver> driverList = mainDAO.getAllDrivers();
         List<HistoricalJourney> journeys = mainDAO.getAllJourneys();
         List<RealTimeJourney> realTimeJourneys = mainDAO.getAllRealTimeJourney();
         List<DriverRecord> driverRecords = mainDAO.getAllDriverRecords();
+        DriverRecord rec;
 
         EntityManager em = emf.createEntityManager();
 
 
         //   PERSIST
-//
-//         driver = new Driver("Conor","conor@conor.com", "hunter2", rec);
-//
-//        mainDAO.persist(driver);
-//
-//        Driver driver1 = new Driver("Frank","frank@conor.com", "hunter2");
-//        mainDAO.persist(driver1);
-//
-
-//
-//
-//        Car car = new Car("VW", "13-D-19008", driverList);
-//        mainDAO.persist(car);
-
-//
-//        HistoricalJourney historicalJourney = new HistoricalJourney("20km", "medium");
-//        mainDAO.persist(historicalJourney);
-//
-
-////
-//        DriverRecord rec;
-//        rec = new DriverRecord(journeys);
-//        mainDAO.persist(rec);
-//
-////        Image i1 = new Image("face3", "png");
-////        Image i2 = new Image("face4", "png");
-////        Image i3 = new Image("face5", "png");
-////        mainDAO.persist(i1);
-////        mainDAO.persist(i2);
-////        mainDAO.persist(i3);
-//        for (HistoricalJourney i : journeys) {
-//            i1.setHistoricalJourney(i);
-//            i2.setHistoricalJourney(i);
-//
-//        }
-////        mainDAO.merge(i1);
-////        mainDAO.merge(i2);
 
 
-        //REMOVE
+        Driver driver1 = new Driver("Frank", "frank@conor.com", "hunter2");
+        mainDAO.persist(driver1);
+
+
+        Car car = new Car("VW", "13-D-19008", driverList);
+        mainDAO.persist(car);
+
+
+        HistoricalJourney historicalJourney = new HistoricalJourney("20km", "medium");
+        mainDAO.persist(historicalJourney);
+
+
+        Image i1 = new Image("face3", "png");
+        Image i2 = new Image("face4", "png");
+        Image i3 = new Image("face5", "png");
+        mainDAO.persist(i1);
+        mainDAO.persist(i2);
+        mainDAO.persist(i3);
+
+
+        Driver d = new Driver("George", "gg@gmail.com", "1234");
+        mainDAO.persist(d);
+        RealTimeJourney realTimeJourney = new RealTimeJourney(true, d, journeys.get(0));
+        mainDAO.persist(realTimeJourney);
+
+
+        Car car1 = new Car("BMW", "18-CN-199", driverList);
+        mainDAO.persist(car1);
+
+
+        HistoricalJourney historicalJ = new HistoricalJourney("5", "low");
+        mainDAO.persist(historicalJ);
 
 
         //MERGE
-        ///Driver driver1 = new Driver(4, "John", "completely@email.com", "afwewef");
+        for (HistoricalJourney i : journeys) {
+            i1.setHistoricalJourney(i);
+            i2.setHistoricalJourney(i);
 
-//        em.getTransaction().begin();
-
-//        mainDAO.remove(driver1);
-//        em.close();
-
-        //    Driver driver = em.find(Driver.class, 1);
-//
-//        em.getTransaction().begin();
-//        em.remove(driver);
-//        em.getTransaction().commit();
-//        Driver d = new Driver("George", "gg@gmail.com", "1234");
-//        mainDAO.persist(d);
-//        RealTimeJourney realTimeJourney = new RealTimeJourney(true, d, journeys.get(0));
-//        mainDAO.persist(realTimeJourney);
-//
-//        List<Car> cars = mainDAO.getCarbyreg("13-D-19008");
-//        for (Car c : cars) {
-//            c.setMake("Audi");
-//            mainDAO.merge(c);
-//        }
-
-//        Car car = new Car("BMW", "18-CN-199", driverList);
-//        mainDAO.persist(car);
-
-//        Car car1 = em.find(Car.class, 2);
-//        em.getTransaction().begin();
-//        em.remove(car1);
-//        em.getTransaction().commit();
-
-//        List<Driver> drivers = mainDAO.getDriverByName("Frank");
-//        for (Driver driver1 : drivers) {
-//         driver1.setEmail("frankie@yahoo.ie");
-//         mainDAO.merge(driver1);
-//        }
+        }
+        mainDAO.merge(i1);
+        mainDAO.merge(i2);
 
 
-//        List<HistoricalJourney> historicalJourneys = mainDAO.getJourneybyTirednessLevel("medium");
-//        for (HistoricalJourney h : historicalJourneys) {
-//            h.setTirednessLevel("normal");
-//            mainDAO.merge(h);
-//        }
-
-//        HistoricalJourney historicalJ = new HistoricalJourney("5", "low");
-//        mainDAO.persist(historicalJ);
-//
+        List<HistoricalJourney> historicalJourneys = mainDAO.getJourneybyTirednessLevel("medium");
+        for (HistoricalJourney h : historicalJourneys) {
+            h.setTirednessLevel("normal");
+            mainDAO.merge(h);
+        }
 
 
-//        HistoricalJourney h1 = em.find(HistoricalJourney.class, 4);
-//        em.getTransaction().begin();
-//        em.remove(h1);
-//        em.getTransaction().commit();
-//
-//
+        List<Driver> drivers = mainDAO.getDriverByName("Frank");
+        for (Driver ds : drivers) {
+            ds.setEmail("frankie@yahoo.ie");
+            mainDAO.merge(ds);
+        }
 
-        Image image = em.find(Image.class, 1);
+
+        List<Car> cars = mainDAO.getCarbyreg("13-D-19008");
+        for (Car c : cars) {
+            c.setMake("Audi");
+            mainDAO.merge(c);
+        }
+
+
+
+
+    // rec = mainDAO.getAllDriverRecords().get(0);
+//        mainDAO.persist(rec);
+
+
+        Driver driver = new Driver("Conor", "conor@conor.com", "hunter2");
+
+
+
+        //REMOVE METODS
+        Driver d1 =em.find(Driver.class, 3);
+        em.getTransaction().begin();
+        em.remove(d1);
+        em.getTransaction().commit();
+
+        HistoricalJourney h1 = em.find(HistoricalJourney.class, 3);
+        em.getTransaction().begin();
+        em.remove(h1);
+        em.getTransaction().commit();
+
+
+        Car car2 = em.find(Car.class, 3);
+        em.getTransaction().begin();
+        em.remove(car2);
+        em.getTransaction().commit();
+
+        Driver driverid1 = em.find(Driver.class, 2);
+
+        em.getTransaction().begin();
+        em.remove(driverid1);
+        em.getTransaction().commit();
+
+        Image image = em.find(Image.class, 2);
         em.getTransaction().begin();
         em.remove(image);
         em.getTransaction().commit();
 
+    em.close();
     }
 
 
@@ -188,11 +193,13 @@ public class Test {
         for (Image i : images) {
             System.out.println("Image: " + i.getTitle() + " File Type: " + i.getFileType());
         }
+
+
     }
 
     public static void main(String[] args) {
         new Test();
-        //showAllTables();
-        //showWithVariable();
+        showAllTables();
+        showWithVariable();
     }
 }
